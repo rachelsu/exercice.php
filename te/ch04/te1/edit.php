8982d49be2fb6cb78d6522c97fdbdbdc59305736
rachelsu <rachelsuter1@gmail.com>
2015-03-14 11:08:45 +0100

    Create edit.php
    Création du fichier edit.php

---
+++ te/ch04/te1/edit.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<?php
+/* ---------------------------------------------------------------------
+ *                        Chapitre 4 TE1
+ * Nom du fichier: edit.php
+ * Auteur: Rachel Suter
+ * Date: 14 mars 2015
+ * Description: Te 1
+ * ---------------------------------------------------------------------
+ */
+?>
+
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Chapitre 4 TE1</title>
+</head>
+<body>
+<p>Vous êtes bien arrivés sur la bonne page pour voir et modifier les détails de la ville. Malheureusement cette page est encore en cours de construction. Voulez-vous revenir à la <a href="cities.php">page précédente</a>?</p>
+</body>
+</html> 
