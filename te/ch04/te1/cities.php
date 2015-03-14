eff4813652e255636e8bb2ba23eecda9d67a3817
rachelsu <rachelsuter1@gmail.com>
2015-03-14 11:08:12 +0100

    Create cities.php
    Nouvelle version du cities.php

---
+++ te/ch04/te1/cities.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<?php
+/* ---------------------------------------------------------------------
+ *                        Chapitre 4 TE1
+ * Nom du fichier: cities.php
+ * Auteur: Rachel Suter
+ * Date: 14 mars 2015
+ * Description: Te 1
+ * ---------------------------------------------------------------------
+ */
+// Pour se connecter à la base de données
+const DB_SERVER = 'localhost';
+const DB_USER   = 'cpnv';
+const DB_PWD    = 'cpnv1234';
+const DB_NAME   = 'world';
+?>
+
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Chapitre 4 TE1</title>
+</head>
+<body>
+<?php
+
+//Connexion à la base de données
+    // Connexion à la base de données
+    $mysqli = new mysqli(DB_SERVER, DB_USER, DB_PWD, DB_NAME);
+
+    //Gestion d'erreur de la connexion à la base de données
+    if ($mysqli->connect_errno) {
+        die("Problème de connexion ({$mysqli->connect_errno}) " . 
+                                     $mysqli->connect_error);
+    }
+
+//Message affichant le nombre de villes en Suisse
+    // Requête pour l'affichage des villes en Suisse
+    if ($result = $mysqli->query("SELECT ID, Name, CountryCode, District FROM City WHERE CountryCode = 'CHE' ORDER BY Name")){
+
+    //Gestion des erreurs lors de la transmission de la requête pour l'affichage des villes en Suisse
+    if (!$result) {
+        $message  = 'Votre requête est invalide: ' . mysqli_error() . "\n";
+        $message .= 'Votre requête complète est: ' . $query;
+        die($message);
+    }
+
+    //Affichage du nombre de villes en Suisse
+        $nbr = $result->num_rows;
+        echo "Il y a " .$nbr. " villes en Suisse. \n";
+        $result->close();
+    }
+
+//Tableau avec les villes de Suisse
+    //Création et affichage du tableau contenant les villes et leurs informations
+    ?>
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>CountryCode</th>
+        <th>District</th>
+        <th>Edit</th>
+      </tr>
+    <?php
+    //Requête pour récupérer les infos 
+    $query = "SELECT ID, Name, CountryCode, District FROM City WHERE CountryCode = 'CHE' ORDER BY Name";
+
+    if ($result = $mysqli->query($query)) {
+     
+        //Gestion des erreurs lors de la transmission de la requête
+        if (!$result) {
+            $message  = 'Requête invalide : ' . mysqli_error() . "\n";
+            $message .= 'Requête complète : ' . $query;
+            die($message);
+        }
+       
+        //Création de la boucle permettant d'afficher tous les enregistrements
+        while ($city = $result->fetch_assoc()) { ?>
+            <tr>
+              <td> <?= $city['ID']; ?> </td>
+              <td> <?= $city['Name']; ?> </td>
+              <td> <?= $city['CountryCode']; ?> </td>
+              <td> <?= $city['District']; ?> </td>
+              <td> <a href="edit.php">Edit</a> </td>
+        <?php
+        }
+        $result->free();
+    }    
+    //Fermeture de la connexion à la base de données
+    $mysqli->close();
+    ?>
+            </tr>
+    </table>
+</body>
+</html>
